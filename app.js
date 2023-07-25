5f213ffbc2b26a90b60e07c73c815a01bf55ca92
Ananth Madhekar <ananth.madhekar@realpage.com>
2023-07-25 18:27:41 +0530

    changes in packagejson and appjs

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+//JSX (transpiled before it reaches the JS) - PARCEL -Babel
+const heading = React.createElement("h1", {id: "heading"}, "React Sample ");
 
-const heading = React.createElement(
-    "h1",
-    {id: "heading"},
-    "Hello World from React!");
+//JSX - is not HTML in JS, its HTML-like or XML-like syntax
 
-console.log(heading); // object
+//JSX => Babel transpiles it to React.createElement = > ReactElement - JS Object => HTMLElement(render)
+const jsxHeading = <h1 id="heading">React using JSX</h1>; //- single line
+const Title = () => { 
+    return (<h1 id="heading">React using JSX</h1>);
+};
 
-const root  = ReactDOM.createRoot(document.getElementById("root"));
+const head = (
+    <head className="head" tabIndex="5">Head Comp</head>
+)
 
-root.render(heading);
+//React Functional Component
+//Component Composition
+const BodyComponent = () =>{
+    return(
+        <div id="container">
+            {head}
+            <Title /> 
+            
+            <article>
+                <h1>My First Component</h1>
+                <ol>
+                    <li>Components: UI Building Blocks</li>
+                    <li>Defining a Component</li>
+                    <li>Using a Component</li>
+                </ol>
+            </article>
+        </div>
+)};
+
+const Comp =() => {
+    return (<h1> As usual we can call the function using component call</h1>);
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<BodyComponent />);
